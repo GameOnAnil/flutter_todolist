c4cdac24d3b2ed35fcae72734f94144e26e983a3
gameonanil <gameonanil@gmail.com>
2022-02-18 17:14:08 +0545

    Add category page added

---
+++ lib/screens/add_category_page.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/model/category.dart';
+import 'package:todo_app/provider/add_category_notifier.dart';
+import 'package:todo_app/provider/category_provider.dart';
+
+final addCategoryProvider =
+    ChangeNotifierProvider<AddCategoryNotifier>((_) => AddCategoryNotifier());
+
+class AddCategoryPage extends ConsumerStatefulWidget {
+  const AddCategoryPage({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<AddCategoryPage> createState() => _AddCategoryPageState();
+}
+
+class _AddCategoryPageState extends ConsumerState<AddCategoryPage> {
+  final formKey = GlobalKey<FormState>();
+  late TextEditingController _titleController;
+
+  @override
+  void initState() {
+    super.initState();
+    var provider = ref.read(addCategoryProvider);
+    _titleController = TextEditingController(text: provider.title);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 70,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.grey.withOpacity(0.7), width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      onChanged: (value) {
+                        ref.read(addCategoryProvider).setTitle(value);
+                      },
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter Title",
+                      ),
+                      style: const TextStyle(fontSize: 15),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Title cannot be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    ColoredBox(
+                      color: Colors.pink,
+                    ),
+                    ColoredBox(
+                      color: Colors.blue,
+                    ),
+                    ColoredBox(
+                      color: Colors.green,
+                    ),
+                    ColoredBox(
+                      color: Colors.indigo,
+                    ),
+                    ColoredBox(
+                      color: Colors.yellow,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15),
+                Consumer(builder: (context, ref, child) {
+                  return SizedBox(
+                      height: 45,
+                      width: MediaQuery.of(context).size.width * 0.75,
+                      child: ElevatedButton(
+                          onPressed: () async {
+                            if (formKey.currentState!.validate()) {
+                              var title =
+                                  ref.read(addCategoryProvider.notifier).title;
+                              var color =
+                                  ref.read(addCategoryProvider.notifier).color;
+
+                              await ref
+                                  .read(categoryProvider.notifier)
+                                  .insertCategory(Category(
+                                      id: null,
+                                      title: title,
+                                      color: color,
+                                      total: 0,
+                                      completed: 0));
+                              Navigator.of(context).pop();
+                            }
+                          },
+                          child: const Text("Save")));
+                })
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ColoredBox extends StatelessWidget {
+  final Color color;
+  const ColoredBox({
+    Key? key,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(builder: (context, ref, child) {
+      return GestureDetector(
+        onTap: () {
+          ref.read(addCategoryProvider).setColor(color.value);
+        },
+        child: Container(
+          height: 55,
+          width: 55,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: color,
+              border: color.value == ref.watch(addCategoryProvider).color
+                  ? Border.all(width: 2, color: Colors.black)
+                  : null),
+        ),
+      );
+    });
+  }
+}
